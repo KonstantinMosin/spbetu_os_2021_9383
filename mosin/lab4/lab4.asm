454a737dc6dfdeb9559a153c09113bb511cc8de4
KonstantM <msnknstntn@gmail.com>
2021-06-18 16:12:55 +0300

    fix

--- mosin/lab4/lab4.asm
+++ mosin/lab4/lab4.asm
@@ -1,11 +1,10 @@
 stack_ segment stack
-	dw 128 dup(?)
+	dw 256 dup(?)
 stack_ ends
 
 data_ segment
-	is_interruption_load db 'Interruption_already_load',0dh,0ah,0dh,0ah,'$'
+	interruption_already_load db 'Interruption_already_load',0dh,0ah,0dh,0ah,'$'
 	interruption_load db 'Interruption_load',0dh,0ah,0dh,0ah,'$'
-	interruption_not_load db 'Interruption_not_load',0dh,0ah,0dh,0ah,'$'
 	interruption_delete db 'Interruption_was_delete',0dh,0ah,0dh,0ah,'$'
 data_ ends
 
@@ -15,26 +14,26 @@ code_ segment
 rout proc far
 	jmp body
 	
-	keep_cs dw 0
+	int_seg dw 256 dup(0)
+	int_sig dw 0ffffh
 	keep_ip dw 0
+	keep_cs dw 0
 	keep_psp dw 0
+	keep_ax dw 0
 	keep_ss dw 0
 	keep_sp dw 0
-	keep_ax dw 0
 	int_counter db 'interruption_counter: 0000$'
-	int_sig dw 9999h
-	int_seg dw 16 dup(?)
 
 body:
-	mov keep_sp,sp
 	mov keep_ax,ax
-	mov ax,ss
-	mov keep_ss,ax
+	mov keep_sp,sp
+	mov keep_ss,ss
 	
-	mov ax,keep_ax
-	mov sp,offset body
 	mov ax,seg int_seg
 	mov ss,ax
+	mov ax,offset int_seg
+	add ax,256
+	mov sp,ax
 	
 	push ax
 	push cx
@@ -105,8 +104,8 @@ update:
 	mov al,20h
 	out 20h,al
 	iret
-rout_end_ptr:
 rout endp
+rout_end_ptr:
 
 setCurs proc
 	mov ah,02h
@@ -124,90 +123,42 @@ getCurs proc
 	ret
 getCurs endp
 
-deleteRout proc
-	cli
-	push ds
-	push es
-	
-	mov ah,35h
-	mov al,1ch
-	int 21h
-	
-	mov si,offset keep_ip
-	sub si,offset rout
-	mov dx,es:[bx + si]
-	mov ax,es:[bx + si + 2]
-	mov ds,ax
-	
-	mov ah,25h
-	mov al,1ch
-	int 21h
-	
-	mov ax,es:[bx + si + 4]
-    mov es,ax
-    push es
-	
-	mov ax,es:[2ch]
-    mov es,ax
-    mov ah,49h
-    int 21h
-	
-	pop es
-	mov ah,49h
-	int 21h
-	
-	pop es
-	pop ds
-	sti
-	
-	mov dx,offset interruption_delete
-	call print
-	ret
-deleteRout endp
+is_interruption_load proc far
+	push bx
+	push si
 
-print proc near
+	mov ah, 35h
+	mov al, 1ch
+	int 21h
+	
+	mov si, offset int_sig
+	sub si, offset rout
+	mov dx, es:[bx + si]
+	cmp dx, int_sig
+	jne not_loaded
+	mov ax, 1h
+    jmp is_loaded_exit
+
+not_loaded:
+    mov ax, 0h
+
+is_loaded_exit:
+	pop si
+	pop bx
+
+    ret
+is_interruption_load endp
+
+load_interruption proc far
 	push ax
-	mov ah,09h
-	int 21h
-	pop ax
-	ret
-print endp
+    push bx
+    push cx
+    push dx
+    push es
+    push ds
 
-main proc far
-	mov ax,data_
-	mov ds,ax
-	
-	push es
-	
-	mov ah,35h
-	mov al,1ch
-	int 21h
-	
-	mov si,offset int_sig
-	sub si,offset rout
-	mov dx,es:[bx + si]
-	cmp dx,int_sig
-	jne interruption_not_loaded
-	
-	pop es
-	
-	mov al,es:[81h+1]
-	cmp al,'/'
-	jne bad_cfg
-	
-	mov al,es:[81h+2]
-	cmp al,'u'
-	jne bad_cfg
-	
-	mov al,es:[81h+3]
-	cmp al,'n'
-	jne bad_cfg
 
-	call deleteRout
-	jmp exit
 
-interruption_not_loaded:
-	mov keep_psp,es
 	mov ah,35h
 	mov al,1ch
 	int 21h
@@ -215,12 +166,9 @@ interruption_not_loaded:
 	mov keep_cs,es
 	mov keep_ip,bx
 	
-	push es
-	push bx
-	push ds
-	
-	lea dx,rout
-	mov ax,seg rout
+
+	mov dx, offset rout
+	mov ax, seg rout
 	mov ds,ax
 	
 	mov ah,25h
@@ -228,13 +176,11 @@ interruption_not_loaded:
 	int 21h
 	
 	pop ds
-	pop bx
-	pop es
-	
+
 	mov dx,offset interruption_load
 	call print
 	
-	lea dx,rout_end_ptr
+	mov dx, offset rout_end_ptr
 	mov cl,4h
 	shr dx,cl
 	inc dx
@@ -244,10 +190,130 @@ interruption_not_loaded:
 	
 	mov ah,31h
 	int 21h
+
+
+	pop es
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+	ret
+load_interruption endp
+
+delete_interruption proc
+	cli
+    
+    push ax
+    push bx
+    push dx
+    push ds
+    push es
+    push si
+
+    mov ah, 35h
+    mov al, 1ch
+    int 21h
+    mov si, offset keep_ip
+    sub si, offset rout
+    mov dx, es:[bx + si]
+    mov ax, es:[bx + si + 2]
+
+    push ds
+    mov ds, ax
+    mov ah, 25h
+    mov al, 1ch
+    int 21h
+    pop ds
+
+    mov ax, es:[bx + si + 4]
+    mov es, ax
+    push es
+    mov ax, es:[2ch]
+    mov es, ax
+    mov ah, 49h
+    int 21h
+    pop es
+    mov ah, 49h
+    int 21h
+
+    sti
+
+	push dx
+	mov dx,offset interruption_delete
+	call print
+	pop dx
+
+    pop si
+    pop es
+    pop ds
+    pop dx
+    pop bx
+    pop ax
+	ret
+delete_interruption endp
+
+print proc near
+	push ax
+	mov ah,09h
+	int 21h
+	pop ax
+	ret
+print endp
+
+check_cmd proc far
+	push es
+	mov ax, keep_psp
+    mov es, ax
+
+	
+    mov al, es:[81h+1]
+	cmp al, '/'
+	jne set_zero
+	
+	mov al, es:[81h+2]
+	cmp al, 'u'
+	jne set_zero
+	
+	mov al, es:[81h+3]
+	cmp al, 'n'
+	jne set_zero
+
+    mov ax, 1h
+    jmp check_cmd_exit
+
+set_zero:
+    mov ax, 0h
+
+check_cmd_exit:
+	pop es
+    ret
+check_cmd endp
+
+main proc far
+	mov ax,data_
+	mov ds,ax
+	mov keep_psp,es
+	
+	push es
+	
+	call is_interruption_load
+	cmp ax,0h
+	jne check_cmd_line
+	
+	call load_interruption
+	pop es
+	jmp exit
+	
+check_cmd_line:
+	pop es
+	call check_cmd
+	cmp ax,0h
+	je already_load
+	call delete_interruption
 	jmp exit
 
-bad_cfg:
-	mov dx,offset is_interruption_load
+already_load:
+	mov dx,offset interruption_already_load
 	call print
 
 exit:
